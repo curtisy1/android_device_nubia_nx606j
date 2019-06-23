1cdc9a8675859fde39042bba40226ef910a71235
curtisy <lausklaus44@gmail.com>
2019-06-23 19:22:45 +0200

    Remove A/B stuff

--- lineage_nx606j.mk
+++ lineage_nx606j.mk
@@ -29,24 +29,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
 
-# A/B
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    system \
-    vbmeta
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script
-
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService
